428fd463e4c74c5705e8af36586a8f8ce703a6fb
Your Name <you@example.com>
2021-08-06 14:04:28 +0800

    AngularCRUD

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EmployeeDashboardComponent } from './employee-dashboard/employee-dashboard.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmployeeDashboardComponent
   ],
   imports: [
     BrowserModule,
+     FormsModule,
+        ReactiveFormsModule,
+        HttpClientModule,
     AppRoutingModule
   ],
   providers: [],
